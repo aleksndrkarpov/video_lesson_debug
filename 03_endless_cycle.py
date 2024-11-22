c7306aae7aeb74b3c8490917450ee0bd95672466
Aleksandr Karpov <aleksndrkarpov@gmail.com>
2024-11-22 15:16:22 +0300

    fix_03

--- 03_endless_cycle.py
+++ 03_endless_cycle.py
@@ -9,5 +9,7 @@ if __name__ == '__main__':
     while True:
         a = randint(-100, 100)
         b = randint(-10, 10)
+        if b == 0:
+            break
         result = division(a, b)
         print(result)
